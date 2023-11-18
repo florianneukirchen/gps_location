bc4201400b94f9f7cb3e708cd7ed6fcb00a1910f
Florian Neukirchen <mail@riannek.de>
2023-11-18 15:14:53 +0100

    Add UTM projection

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-
+import 'package:proj4dart/proj4dart.dart';
 
 
 
@@ -119,6 +119,8 @@ class _MyPositionPageState extends State<MyPositionPage> {
     );
   }
 
+
+
   @override
   Widget build(BuildContext context) {
     if (currentposition == null) {
@@ -141,10 +143,8 @@ class _MyPositionPageState extends State<MyPositionPage> {
       return Center(
         child: Column(
           children: <Widget>[
-            const Text(
-              'Current Location: ',
-            ),
             ShowLocationWGS84(position: currentposition!),
+            ShowLocationUTM(position: currentposition!)
           ],
         ),
       );
@@ -204,3 +204,91 @@ class ShowLocationWGS84 extends StatelessWidget {
         ));
   }
 }
+
+
+class ShowLocationUTM extends StatelessWidget {
+  const ShowLocationUTM({
+    super.key,
+    required this.position,
+  });
+
+  final Position position;
+
+  int getUTMzone(Position position) {
+    int utmzone = ((position.longitude + 180) / 6).floor() + 1;
+
+    // Southern Norway
+    if (position.latitude >= 72.0 && position.latitude < 64.0 && position.longitude >= 3.0 && position.latitude < 12.0) {
+      utmzone = 32;
+    }
+
+    // Svalbart
+    if (position.latitude >= 72.0 && position.longitude < 84.0 ) {
+      if (position.longitude >= 0.0 && position.longitude < 9.0) {
+        utmzone = 31;
+      } else if (position.longitude >= 9.0 && position.longitude < 21.0) {
+        utmzone = 33;
+      } else if (position.longitude >= 21.0 && position.longitude < 33.0) {
+        utmzone = 35;
+      } else if (position.longitude >= 33.0 && position.longitude < 42.0){
+        utmzone = 37;
+      }
+    }
+
+    return utmzone;
+  }
+
+  (Point, String) reprojectUTM(Position position, int utmzone, {bool etrs89 = false}) {
+    var pointSrc = Point(x: position.latitude, y: position.longitude);
+
+    // WGS84
+    var epsg = 'EPSG:326$utmzone';
+    String projstring = "+proj=utm +zone=$utmzone +datum=WGS84 +units=m +no_defs +type=crs";
+
+    if (etrs89) {
+      epsg = 'EPSG:258$utmzone';
+      projstring = "+proj=utm +zone=$utmzone +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs";
+    }
+
+    // Set up proj (proj4dart only has few named projections)
+    var projDst = Projection.add(epsg, projstring);
+    var projSrc = Projection.get('EPSG:4326')!;
+    var pointDst = projSrc.transform(projDst, pointSrc);
+
+    return (pointDst, epsg);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final utmzone = getUTMzone(position);
+    final (pointutm, epsg) = reprojectUTM(position, utmzone);
+    return Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Text("UTM Zone $utmzone N ($epsg)"),
+              // Text(pointutm.toString()),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text("X: " + pointutm.x.toStringAsFixed(1) + " m"),
+                  Text("Y: " + pointutm.y.toStringAsFixed(1) + " m"),
+                  Text("(± " + position.accuracy.toStringAsFixed(1) + " m)"),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text("Altitude: " + position.altitude.toString() + " m"),
+                  Text("± " + position.altitudeAccuracy.toString() + " m"),
+                ],
+              ),
+              Text("Timestamp: " + position.timestamp.toString()),
+            ],
+          ),
+        ));
+  }
+}
+
+
