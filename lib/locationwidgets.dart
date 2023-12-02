8e280701d267f9d14ae71a106ca4f98f1c4583c8
Florian Neukirchen <mail@riannek.de>
2023-12-02 21:26:23 +0100

    Fix reprojection to UTM, lat and lon was switched

--- lib/locationwidgets.dart
+++ lib/locationwidgets.dart
@@ -192,51 +192,6 @@ class ShowLocationUTM extends StatelessWidget {
 
   final Position position;
 
-  int getUTMzone(Position position) {
-    int utmzone = ((position.longitude + 180) / 6).floor() + 1;
-
-    // Southern Norway
-    if (position.latitude >= 72.0 && position.latitude < 64.0 && position.longitude >= 3.0 && position.latitude < 12.0) {
-      utmzone = 32;
-    }
-
-    // Svalbart
-    if (position.latitude >= 72.0 && position.longitude < 84.0 ) {
-      if (position.longitude >= 0.0 && position.longitude < 9.0) {
-        utmzone = 31;
-      } else if (position.longitude >= 9.0 && position.longitude < 21.0) {
-        utmzone = 33;
-      } else if (position.longitude >= 21.0 && position.longitude < 33.0) {
-        utmzone = 35;
-      } else if (position.longitude >= 33.0 && position.longitude < 42.0){
-        utmzone = 37;
-      }
-    }
-
-    return utmzone;
-  }
-
-  (Point, String) reprojectUTM(Position position, int utmzone, {bool etrs89 = false}) {
-    var pointSrc = Point(x: position.latitude, y: position.longitude);
-
-    // WGS84
-    var epsg = 'EPSG:326$utmzone';
-    String projstring = "+proj=utm +zone=$utmzone +datum=WGS84 +units=m +no_defs +type=crs";
-
-    if (etrs89) {
-      epsg = 'EPSG:258$utmzone';
-      projstring = "+proj=utm +zone=$utmzone +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs";
-    }
-
-    // Set up proj (proj4dart only has few named projections)
-    // Get it by name if it exists, else create and add it
-    var projDst = Projection.get(epsg) ?? Projection.add(epsg, projstring);
-    var projSrc = Projection.get('EPSG:4326')!;
-    var pointDst = projSrc.transform(projDst, pointSrc);
-
-    return (pointDst, epsg);
-  }
-
   @override
   Widget build(BuildContext context) {
     final utmzone = getUTMzone(position);
@@ -251,8 +206,8 @@ class ShowLocationUTM extends StatelessWidget {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Text("X: " + pointutm.x.toStringAsFixed(1) + " m"),
-                  Text("Y: " + pointutm.y.toStringAsFixed(1) + " m"),
+                  Text("X: " + pointutm.x.toStringAsFixed(0) + " m"),
+                  Text("Y: " + pointutm.y.toStringAsFixed(0) + " m"),
                   Text("(± " + position.accuracy.toStringAsFixed(0) + " m)"),
                 ],
               ),
@@ -276,3 +231,48 @@ class ShowLocationUTM extends StatelessWidget {
   }
 }
 
+int getUTMzone(Position position) {
+  int utmzone = ((position.longitude + 180) / 6).floor() + 1;
+
+  // Southern Norway
+  if (position.latitude >= 72.0 && position.latitude < 64.0 && position.longitude >= 3.0 && position.latitude < 12.0) {
+    utmzone = 32;
+  }
+
+  // Svalbart
+  if (position.latitude >= 72.0 && position.longitude < 84.0 ) {
+    if (position.longitude >= 0.0 && position.longitude < 9.0) {
+      utmzone = 31;
+    } else if (position.longitude >= 9.0 && position.longitude < 21.0) {
+      utmzone = 33;
+    } else if (position.longitude >= 21.0 && position.longitude < 33.0) {
+      utmzone = 35;
+    } else if (position.longitude >= 33.0 && position.longitude < 42.0){
+      utmzone = 37;
+    }
+  }
+  return utmzone;
+}
+
+(Point, String) reprojectUTM(Position position, int utmzone, {bool etrs89 = false}) {
+  var pointSrc = Point(x: position.longitude, y: position.latitude);
+
+  // WGS84
+  var epsg = 'EPSG:326$utmzone';
+  String projstring = "+proj=utm +zone=$utmzone";
+  // "+proj=utm +zone=$utmzone +datum=WGS84 +no_defs +type=crs"
+  print(projstring);
+
+  if (etrs89) {
+    epsg = 'EPSG:258$utmzone';
+    projstring = "+proj=utm +zone=$utmzone +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs";
+  }
+
+  // Set up proj (proj4dart only has few named projections)
+  // Get it by name if it exists, else create and add it
+  var projDst = Projection.get(epsg) ?? Projection.add(epsg, projstring);
+  var projSrc = Projection.get('EPSG:4326')!;
+  var pointDst = projSrc.transform(projDst, pointSrc);
+
+  return (pointDst, epsg);
+}
